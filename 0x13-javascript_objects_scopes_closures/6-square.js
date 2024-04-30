a4b82d889b4cdde0dc1e060b1671654fadcbc54b
Cherifi hiba <cherifi.hiba.2019@gmail.com>
2024-04-30 11:37:42 -0700

    Done

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+'use strict';
+
+const ParentSquare = require('./5-square');
+
+class Square extends ParentSquare {
+  charPrint (c = 'X') {
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Square;
