0f1ccb56a70c8ec45379f761233bc352f06d871a
Cherifi hiba <cherifi.hiba.2019@gmail.com>
2024-04-30 12:58:23 -0700

    Done

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+'use strict';
+
+const dict = require('./101-data').dict;
+
+const newDict = {};
+
+for (const key in dict) {
+  if (dict.hasOwnProperty(key)) {
+    const value = dict[key];
+    if (newDict[value] === undefined) {
+      newDict[value] = [key];
+    } else {
+      newDict[value].push(key);
+    }
+  }
+}
+
+console.log(newDict);
