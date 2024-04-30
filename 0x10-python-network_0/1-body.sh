fa0dd6e39611bf0869c2e752f97380ee5826cd71
Cherifi hiba <cherifi.hiba.2019@gmail.com>
2024-04-30 05:00:24 -0700

    Done

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
-# This script takes in a URL, sends a GET request to the URL, and displays the body of the response
-# Display only the body of a 200 status code response
-
-curl -sL -w "%{http_code}" "$1" -o /dev/null | { 
-    IFS= read -r status_code
-    if [ "$status_code" -eq 200 ]; then
-        curl -sL "$1"
-    fi
-}
-
+# This Bash script takes in a URL, sends a GET request to the URL, and displays the body of the response
+curl -sL $1
