8912f342a733a78d7b080cc6b2ee7e91ae439839
Cherifi hiba <cherifi.hiba.2019@gmail.com>
2024-04-30 05:47:56 -0700

    Done

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 # This Bash script sends a JSON POST request to a URL passed as the first argument, and displays the body of the response
-file_content=$(cat $2 2>/dev/null)
+file_content=$(cat "$2" 2>/dev/null)
 if [ $? -eq 0 ]; then
     echo "$file_content" | jq empty 2>/dev/null
     if [ $? -eq 0 ]; then
         curl -sX POST -H "Content-Type: application/json" -d @"$2" $1
     else
         echo "Not a valid JSON"
+        exit 1
     fi
 else
     echo "Could not read file"
+    exit 1
 fi
