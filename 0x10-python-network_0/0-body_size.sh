c409b56c052c01455bd3b5e39179372a45ed8858
Cherifi hiba <cherifi.hiba.2019@gmail.com>
2024-04-30 03:12:54 -0700

    Done

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-# This script takes in a URL, sends a request to that URL, and displays the size of the body of the response in bytes
-# The size is displayed in bytes
-curl -sI "$1" | grep -i Content-Length | awk '{print $2}' | tr -d '\n\r'
+# This script takes a URL as an argument, sends a request to that URL using curl,
+# and displays the size of the response body in bytes.
+
+# Check if a URL is provided as an argument
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <URL>"
+    exit 1
+fi
+
+# Send a request to the provided URL and display the size of the response body
+curl -sI "$1" | grep -i Content-Length | awk '{print $2}' | tr -d '\r'
 
