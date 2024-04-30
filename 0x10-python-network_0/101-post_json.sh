08d377fd334f3f1066b39ac7cede76ee38e8d5bc
Cherifi hiba <cherifi.hiba.2019@gmail.com>
2024-04-30 05:52:07 -0700

    Done

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Sends a JSON POST request to a URL passed as the first argument,
 # with the contents of a file passed as the second argument
-curl -sX POST -H "Content-Type: application/json" -d @"$2" "$1"
+curl -sX POST -H "Content-Type: application/json" -d @"$2" "$1" | awk '/Valid JSON/{p=1;next} p' | sed 's/Valid JSON//g'
