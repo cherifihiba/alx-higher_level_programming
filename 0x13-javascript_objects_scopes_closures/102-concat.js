f654a6595e5a8650f2e4fd96508b8880cd536675
Cherifi hiba <cherifi.hiba.2019@gmail.com>
2024-04-30 13:26:20 -0700

    Done

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -4,11 +4,20 @@ const fs = require('fs');
 const [,, fileA, fileB, fileC] = process.argv;
 
 fs.readFile(fileA, 'utf8', (err, dataA) => {
-  if (err) throw err;
+  if (err) {
+    console.error(err);
+    return;
+  }
   fs.readFile(fileB, 'utf8', (err, dataB) => {
-    if (err) throw err;
+    if (err) {
+      console.error(err);
+      return;
+    }
     fs.writeFile(fileC, dataA.trim() + '\n' + dataB.trim() + '\n', (err) => {
-      if (err) throw err;
+      if (err) {
+        console.error(err);
+        return;
+      }
       console.log('The files have been concatenated successfully!');
     });
   });
