ce6d00d4f4d296270bd957b6a5e4146d9d7faefe
Cherifi hiba <cherifi.hiba.2019@gmail.com>
2024-04-30 10:58:24 -0700

    Done

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -3,8 +3,8 @@
 
 class Rectangle {
   constructor(w, h) {
-    if (w === undefined || h === undefined || w <= 0 || h <= 0 || typeof w !== 'number' || typeof h !== 'number') {
-      return;
+    if (w <= 0 || h <= 0 || typeof w !== 'number' || typeof h !== 'number') {
+      return {};
     }
     this.width = w;
     this.height = h;
