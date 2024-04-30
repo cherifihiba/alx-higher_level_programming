ac4f6c06bf076ea3d7e84f68c205732d6d85ccff
Cherifi hiba <cherifi.hiba.2019@gmail.com>
2024-04-30 13:10:17 -0700

    Done

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -7,9 +7,13 @@ const sourceFile1 = process.argv[2];
 const sourceFile2 = process.argv[3];
 const destinationFile = process.argv[4];
 
-const data1 = fs.readFileSync(sourceFile1, 'utf8');
-const data2 = fs.readFileSync(sourceFile2, 'utf8');
-
-fs.writeFileSync(destinationFile, data1 + data2);
-
-console.log(`The content of ${sourceFile1} and ${sourceFile2} has been concatenated and written to ${destinationFile}`);
+try {
+  const data1 = fs.readFileSync(sourceFile1, 'utf8');
+  const data2 = fs.readFileSync(sourceFile2, 'utf8');
+  
+  fs.writeFileSync(destinationFile, data1 + data2);
+  
+  console.log(`The content of ${sourceFile1} and ${sourceFile2} has been concatenated and written to ${destinationFile}`);
+} catch (error) {
+  console.error(error.message);
+}
