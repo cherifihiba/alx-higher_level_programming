33420b7219c364964f7b12ea5da3dc2175260f5e
Cherifi hiba <cherifi.hiba.2019@gmail.com>
2024-04-30 13:30:12 -0700

    Done

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,26 +1,17 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-const { promisify } = require('util');
-
-const readFile = promisify(fs.readFile);
-const appendFile = promisify(fs.appendFile);
-
-async function concatFiles(source1, source2, destination) {
-  try {
-    const data1 = await readFile(source1, 'utf8');
-    const data2 = await readFile(source2, 'utf8');
-    await appendFile(destination, data1 + data2);
-    console.log('Files concatenated successfully!');
-  } catch (error) {
-    console.error('An error occurred:', error);
-  }
-}
 
 if (process.argv.length !== 5) {
-  console.error('Usage: ./concat_files.js <source1> <source2> <destination>');
+  console.error('Usage: ./102-concat.js <source1> <source2> <destination>');
   process.exit(1);
 }
 
 const [, , source1, source2, destination] = process.argv;
-concatFiles(source1, source2, destination);
+
+const data1 = fs.readFileSync(source1, 'utf8');
+const data2 = fs.readFileSync(source2, 'utf8');
+
+fs.writeFileSync(destination, data1 + data2);
+
+console.log('Files concatenated successfully!');
