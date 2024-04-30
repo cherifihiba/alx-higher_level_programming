bca56ee570cd0ae28c446d47185ac419367c8b1f
Cherifi hiba <cherifi.hiba.2019@gmail.com>
2024-04-30 05:55:33 -0700

    Done

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -2,4 +2,4 @@
 # Sends a JSON POST request to a URL passed as the first argument,
 # with the contents of a file passed as the second argument
 response=$(curl -sX POST -H "Content-Type: application/json" -d @"$2" "$1")
-echo "$response" | grep -q "Valid JSON" && echo "$response" || echo "Not a valid JSON"
+echo "$response" | grep -q "Not a valid JSON" && echo "Not a valid JSON" || echo "$response"
