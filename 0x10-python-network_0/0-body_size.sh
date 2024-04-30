5b4d4b2619bdc27abebee345052bd89052d86249
Cherifi hiba <cherifi.hiba.2019@gmail.com>
2024-04-30 03:26:50 -0700

    Done

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # This script takes in a URL, sends a request to that URL, and displays the size of the body of the response in bytes
-curl -sI "$1" | grep -i Content-Length | awk '{print $2}' | tr -d '\n'
+curl -sI "$1" | grep -i Content-Length | awk '{print $2}' | tr -d '\n\r'
 
