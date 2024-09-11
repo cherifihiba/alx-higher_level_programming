d8e2a5a8393dd84f83ff8592281349bbbe0f59ce
cherifi hiba <cherifi.hiba.2019@gmail.com>
2024-09-10 20:50:52 -0700

    Done

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
-#!/usr/bin/python3
-for x in range(0, 10):
-    for y in range(x + 1, 10):
-        if x == 8 and y == 9:
-            print('89')
+!/usr/bin/python3
+for first_number in range(0, 10):
+    for second_number in range(first_number + 1, 10):
+        if first_number == 8 and second_number == 9:
+            print("{}{}".format(first_number, second_number))
         else:
-            print('{}{}, '.format(x, y), end='')
+            print("{}{}, ".format(first_number, second_number), end='')
