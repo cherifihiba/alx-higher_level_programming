7c8e09259dd0e1919a93d7fbfc478fd9ce77bb68
cherifi hiba <cherifi.hiba.2019@gmail.com>
2024-09-18 21:27:12 -0700

    Done

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,36 +1,15 @@
 #!/usr/bin/python3
-def roman_to_int(roman_string):
-    if not isinstance(roman_string, str) or not roman_string:
+def roman_to_int(roman_string: str):
+    if roman_string is None or type(roman_string) != str:
         return 0
-    
     data = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    
-    # Check for invalid characters
-    for char in roman_string:
-        if char not in data:
-            return 0
-
-    # Check for invalid repeated numerals (e.g., IIII, VV, etc.)
-    if "IIII" in roman_string or "VV" in roman_string or "XXXX" in roman_string \
-        or "LL" in roman_string or "CCCC" in roman_string or "DD" in roman_string \
-        or "MMMM" in roman_string:
-        return 0
-    
-    # Check for invalid subtractive combinations
-    invalid_combinations = ["IL", "IC", "ID", "IM", "VX", "VL", "VC", "VD", "VM",
-                            "XD", "XM", "LC", "LD", "LM", "DM"]
-    for combination in invalid_combinations:
-        if combination in roman_string:
-            return 0
-    
-    numbers = [data[char] for char in roman_string]
-    total = 0
+    numbers = [data[x] for x in roman_string] + [0]
+    rep = 0
 
     for i in range(len(numbers) - 1):
-        if numbers[i] < numbers[i + 1]:
-            total -= numbers[i]
+        if numbers[i] >= numbers[i+1]:
+            rep += numbers[i]
         else:
-            total += numbers[i]
+            rep -= numbers[i]
 
-    return total + numbers[-1]
-
+    return rep
